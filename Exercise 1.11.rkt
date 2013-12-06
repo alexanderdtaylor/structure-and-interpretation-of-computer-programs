711088c57d4a98fee45e7b33015733b3625cfffe
chenyuzh <ciccyzcy@gmail.com>
2013-12-05 21:12:41 -0500

    sicp

--- Exercise 1.11.rkt
+++ Exercise 1.11.rkt
@@ -9,3 +9,11 @@
       (func-iter (+ (* 3 c) (* 2 b) a) a b (- count 1))))
 
 (func 4)
+
+(define (func-recursive n)
+  (if (< n 3)
+      n
+      (+ (func-recursive (- n 1)) (* 2 (func-recursive (- n 2)))  
+         (* 3 (func-recursive (- n 3))))))
+
+(func-recursive 4)
