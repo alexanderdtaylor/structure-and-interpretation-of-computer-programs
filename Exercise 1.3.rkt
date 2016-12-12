21a3c3bf7100a07346c47e2598adb051ba112862
Alexander Taylor <no-email@gmail.com>
2016-12-12 17:02:23 -0500

    Exercise 1.3 - biggest two, sum of squares

---
+++ Exercise 1.3.rkt
@@ -0,0 +1,35 @@
+; Exercise 1.3 
+; Define a procedure that takes three numbers as arguments 
+; and returns the sum of the squares of the two larger numbers.
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (big-two a b c) 
+	(if (> a b) 
+		(if (> b c) 
+			(sum-of-squares a b) 
+			(sum-of-squares a c)) 
+		(if (> a c)
+			(sum-of-squares a b) 
+			(sum-of-squares b c))))
+
+; psedo-code for Exercise 1.3
+;
+; compare b and c			
+;				then a,b are biggest
+;				sum-of-squares(a b)
+;			if c > b
+;				then a,c are biggest
+;				sum-of-squares(a c)
+;	
+;	if a =< b
+;		;compare a and c
+;			if a >= c
+;				then a,b are biggest
+;				sum-of-squares(a b)
+;			if c > a
+;				then b,c are biggest
+;				sum-of-squares(a b)
